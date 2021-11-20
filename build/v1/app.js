ce8b8957234a947092c5bda2dbad1d999c5787bd
Supertiger <saawalkhn@googlemail.com>
2021-11-20 09:21:41 +0000

    gitignore build folder

--- build/v1/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-var routes_1 = require("./routes/routes");
-Object.defineProperty(exports, "router", { enumerable: true, get: function () { return routes_1.router; } });
