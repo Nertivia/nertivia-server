27094999588e8474f1182601520098bb36bbc625
FluxedScript <fluxedscript@gmail.com>
2021-11-20 12:08:59 +0000

    add 404 error and have a default api

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ import { router as routerV1 } from "./v1/app";
 const server = express();
 
 server.use("/api/v1", routerV1)
+server.use("/api", routerV1) // Use the latest api version as the default
+
+server.all("*", (req, res) => {
+    res.status(404).send({
+        error: "Not found"
+    });
+});
 
 server.listen(80, () => {
   console.log("Listening on port *:80")
