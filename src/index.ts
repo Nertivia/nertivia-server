1871e56d4e030fed9156bc3be65e6aa2a8966c61
Supertiger <saawalkhn@googlemail.com>
2021-11-21 14:37:55 +0000

    changed user dao to a class

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-import express from 'express';
+import express from "express";
 import { router as routerV1 } from "./v1/app";
 
-import './env';
+import "./env";
 
 const server = express();
 
-server.use(express.urlencoded({extended: true})); 
+server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
-server.use("/api/v1", routerV1)
-server.use("/api", routerV1) // Use the latest api version as the default
+server.use("/api/v1", routerV1);
+server.use("/api", routerV1); // Use the latest api version as the default
 
 server.all("*", (req, res) => {
-    res.status(404).send({
-        error: "Not found"
-    });
+  res.status(404).send({
+    error: "Not found",
+  });
 });
 
 server.listen(80, () => {
-  console.log("Listening on port *:80")
-})
+  console.log("Listening on port *:80");
+});
 
 module.exports = server;
--- src/v1/routes/users/createAccount.ts
+++ src/v1/routes/users/createAccount.ts
@@ -1,20 +1,18 @@
-import { Request, Response } from "express"
-import { createUser } from "../../database/user"
-
+import { Request, Response } from "express";
+import user from "../../database/user";
 
 export const createAccount = async (req: Request, res: Response) => {
-  const {email, username, password} = req.body;
+  const { email, username, password } = req.body;
 
-  const [id, error] = await createUser({
+  const [id, error] = await user.createUser({
     email,
     username,
-    password
+    password,
   });
 
   if (error) {
-    return res.status(403).json({error})
+    return res.status(403).json({ error });
   }
-  
-  return res.json({id})
 
-}
+  return res.json({ id });
+};
