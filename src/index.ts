415150786bdd1156dc95b41e35bb2df1f99ee5bd
FluxedScript <fluxedscript@gmail.com>
2021-11-23 23:10:38 +0000

    port env

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { router as routerV1 } from "./v1/app";
 
 import "./env";
+import env from "./env";
 import { connectRedis } from "./redis";
 
 const server = express();
@@ -20,8 +21,8 @@ server.all("*", (req, res) => {
 
 async function startServer() {
   await connectRedis();
-  server.listen(80, () => {
-    console.log("Listening on port *:80");
+  server.listen(env.PORT, () => {
+    console.log(`Listening on port *:${env.PORT}`);
   });
 }
 startServer();
