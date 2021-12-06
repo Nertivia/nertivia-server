ed63decfe3c15e684ba20cc1fc12a75ac5146b9e
supertiger1234 <saawalkhn@googlemail.com>
2021-12-06 12:58:58 +0000

    remove useless 1

--- src/database.ts
+++ src/database.ts
@@ -4,7 +4,7 @@ import { PrismaClient, Prisma } from '@prisma/client'
 const prisma = new PrismaClient();
 
 prisma.$queryRaw(Prisma.sql`SELECT 1`).then(() => {
-  console.log("PostgreSQL connected");1
+  console.log("PostgreSQL connected");
 })
 
 export default prisma;
